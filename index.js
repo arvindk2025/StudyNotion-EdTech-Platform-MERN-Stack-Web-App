260de91878d335a238ab4c5156d99ee15ede0a73
ARVIND KUMAR <arvindnisha2002@gmail.com>
2024-02-05 02:43:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ const store = configureStore({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
         <Toaster />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  
 );
